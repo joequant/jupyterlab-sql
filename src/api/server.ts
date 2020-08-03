bcd59284267d7cea90626498f17adac9ebc95af0
Joseph C Wang <joequant@bitquant.com.hk>
2020-08-03 10:40:21 +0800

    fix read of settings

--- src/api/server.ts
+++ src/api/server.ts
@@ -3,12 +3,11 @@ import { URLExt } from '@jupyterlab/coreutils';
 import { ServerConnection } from '@jupyterlab/services';
 
 export namespace Server {
-  const settings = ServerConnection.defaultSettings;
-
   export async function makeRequest(
     endpoint: string,
     request: RequestInit
   ): Promise<Response> {
+    const settings = ServerConnection.makeSettings();
     const url = URLExt.join(settings.baseUrl, endpoint);
     return await ServerConnection.makeRequest(url, request, settings);
   }
